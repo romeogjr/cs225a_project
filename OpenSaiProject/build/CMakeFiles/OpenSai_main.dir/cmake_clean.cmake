ce0d26f355262d7f89b9eb92079295d27d016595
romeogj-admin <rgj0@stanford.edu>
2025-04-28 20:14:06 -0700

    more files

---
+++ OpenSaiProject/build/CMakeFiles/OpenSai_main.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/Users/romeo/Documents/courses/Spring 2025/CS225A/hw/OpenSai/bin/OpenSai_main"
+  "/Users/romeo/Documents/courses/Spring 2025/CS225A/hw/OpenSai/bin/OpenSai_main.pdb"
+  "CMakeFiles/OpenSai_main.dir/src/main.cpp.o"
+  "CMakeFiles/OpenSai_main.dir/src/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OpenSai_main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ OpenSaiProject/build/CMakeFiles/Tutorials_OpenSai.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/Users/romeo/Documents/courses/Spring 2025/CS225A/hw/OpenSai/bin/Tutorials_OpenSai"
+  "/Users/romeo/Documents/courses/Spring 2025/CS225A/hw/OpenSai/bin/Tutorials_OpenSai.pdb"
+  "CMakeFiles/Tutorials_OpenSai.dir/tutorials/main_tutorials.cpp.o"
+  "CMakeFiles/Tutorials_OpenSai.dir/tutorials/main_tutorials.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Tutorials_OpenSai.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
